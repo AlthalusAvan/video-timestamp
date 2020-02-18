0d66816a70cfc44735639648d90a3a8b62f6dbb8
MacDara Roche <mroche7@tcd.ie>
2020-02-18 11:16:17 +0000

    Finished timestamping tool

---
+++ main.js
@@ -0,0 +1,62 @@
+const fs = require('fs');
+
+const readline = require("readline");
+
+const startTime = new Date();
+
+console.log("Start time: " + startTime.toLocaleTimeString());
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+var timestamps = [];
+
+
+function createTimestamps() {
+    rl.question("Press enter to log a timestamp, or type \"exit\" when you're finished\n", (input) => {
+        if(input === "exit") {
+            rl.close();
+        } else {
+            rl.question("Enter your timestamp description\n", (answer) => {
+                let timeSinceStart = new Date() - startTime;
+    
+                timestamps.push(msToHMS(timeSinceStart) + " - " + answer);
+
+                createTimestamps();
+            });
+        }
+    });
+}
+
+createTimestamps();
+
+rl.on("close", () => {
+    let timeSinceStart = new Date() - startTime;
+    console.log("\nTotal stream time: " + msToHMS(timeSinceStart));
+    
+    fs.writeFile(
+        "./timestamps.txt",
+        timestamps.map(function(v){ return v }).join('\n'),
+        function (err) { console.log(err ? 'Error :'+err : 'ok') }
+   );
+
+});
+
+function msToHMS( ms ) {
+    // 1- Convert to seconds:
+    var seconds = ms / 1000;
+    // 2- Extract hours:
+    var hours = parseInt( seconds / 3600 ); // 3,600 seconds in 1 hour
+    seconds = seconds % 3600; // seconds remaining after extracting hours
+    // 3- Extract minutes:
+    var minutes = parseInt( seconds / 60 ); // 60 seconds in 1 minute
+    // 4- Keep only seconds not extracted to minutes:
+    seconds = Math.round(seconds % 60);
+    return( pad(hours) + ":" + pad(minutes) + ":" + pad(seconds));
+}
+
+function pad(n) {
+    return (n < 10) ? ("0" + n) : n;
+}
